6a6499873a8ad56e77375ed41b51a739240b0a55
闪电侠 <qinchende@qq.com>
2022-03-09 00:01:02 +0800

    example

--- web-demo/route/index.go
+++ web-demo/route/index.go
@@ -1,7 +1,6 @@
 package route
 
 import (
-	"gf-example/web-demo/route/mid"
 	"github.com/qinchende/gofast/fst"
 	"github.com/qinchende/gofast/fstx"
 	"github.com/qinchende/gofast/jwtx"
@@ -30,7 +29,7 @@ func LoadRoutes(app *fst.GoFast) {
 	// Note: 请求进来，并没有定位到具体的路由。就需要走这些过滤器
 	// 所有的请求都要走这里指定的拦截器，发生错误就直接中断返回
 	app.Apply(fstx.DefaultFits) // 默认的一组中间件
-	app.Fit(mid.MyFitDemo)      // 自定义中间件
+	//app.Fit(mid.MyFitDemo)      // 自定义中间件
 	// 2.2. 全局，第二级中间件
 	app.Apply(fstx.DefaultHandlers)
 
