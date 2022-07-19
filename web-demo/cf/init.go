68859b2b805402204ffcbf2307751a6277310097
闪电侠 <qinchende@qq.com>
2022-07-19 22:16:12 +0800

    demo

--- web-demo/cf/init.go
+++ web-demo/cf/init.go
@@ -3,10 +3,10 @@ package cf
 import (
 	"flag"
 	"github.com/qinchende/gofast/connx/gform"
-	"github.com/qinchende/gofast/def"
-	"github.com/qinchende/gofast/def/jwtx"
 	"github.com/qinchende/gofast/fst"
 	"github.com/qinchende/gofast/logx"
+	"github.com/qinchende/gofast/sdx"
+	"github.com/qinchende/gofast/sdx/jwtx"
 	"github.com/qinchende/gofast/skill/conf"
 )
 
@@ -22,7 +22,7 @@ var cnfFile = flag.String("f", "cf/env.yaml", "-f env.[yaml|yml|json]")
 func InitEnvConfig() {
 	flag.Parse()
 	conf.MustLoad(*cnfFile, &AppCnf)
-	def.InitLogger(&AppCnf.WebServerCnf.LogConfig)
+	sdx.InitLogger(&AppCnf.WebServerCnf.LogConfig)
 	logx.Info("Hello " + AppCnf.WebServerCnf.Name + ", Welcome.")
 
 	// initGoRedis()
