4ace48d9dbef7c6144dcc79c347a4d885465987f
闪电侠 <qinchende@qq.com>
2022-08-17 21:41:07 +0800

    refactor

--- web-demo/logic/admin/set.go
+++ web-demo/logic/admin/set.go
@@ -5,6 +5,10 @@ import (
 	"github.com/qinchende/gofast/logx"
 )
 
+func BeforeA(ctx *fst.Context) {
+	logx.Info("Handler crm.BeforeA")
+}
+
 // curl -H "Content-Type: application/json" -X GET --data '{"name":"bmc","account":"rmb","age":37}' http://127.0.0.1:8078/admin/set
 // curl -H "Content-Type: application/json" -X POST --data '{"name":"bmc","account":"rmb","age":37}' http://127.0.0.1:8078/admin/set
 func SetParams(ctx *fst.Context) {
--- web-demo/model/hr/title.go
+++ web-demo/model/hr/title.go
@@ -1,24 +1,18 @@
 package hr
 
 import (
-	"gf-example/web-demo/model"
 	"github.com/qinchende/gofast/store/orm"
 )
 
 type Title struct {
-	model.CommonFields
+	orm.CommonFields
 	Level int16
 	Name  string `pms:"name" v:"required"`
 	Desc  string `pms:"desc" v:""`
 }
 
-func (*Title) GfAttrs() *orm.ModelAttrs {
+func (t *Title) GfAttrs(orm.OrmStruct) *orm.ModelAttrs {
 	return &orm.ModelAttrs{
 		TableName: "sys_title",
-		CacheAll:  true,
 	}
 }
-
-//func (*Title) TableName() string {
-//	return "sys_title"
-//}
