4a0ef182907d965fa9423bf397f1b39dfedc1519
闪电侠 <qinchende@qq.com>
2023-02-23 00:37:56 +0800

    fixed

--- web-demo/logic/admin/set.go
+++ web-demo/logic/admin/set.go
@@ -6,12 +6,12 @@ import (
 )
 
 func BeforeA(c *fst.Context) {
-	c.AddMsgBasket("Handler admin.BeforeA")
+	c.CarryMsg("Handler admin.BeforeA")
 }
 
 // curl -i -H "Content-Type: application/json" -X GET --data '{"name":"bmc","account":"rmb","age":37,"tok":"t:WUFZT3lKZFp5cmtlVkdQRnA2.UANo7oIqAyAw0P4Bwdzs2gcQumjgij2luC2jZrSLPOE"}' http://127.0.0.1:8078/admin/set
 // curl -i -H "Content-Type: application/json" -X POST --data '{"name":"bmc","account":"rmb","age":37}' http://127.0.0.1:8078/admin/set
 func SetParams(c *fst.Context) {
-	c.AddMsgBasket("Handler admin.SetParams")
+	c.CarryMsg("Handler admin.SetParams")
 	c.SucData(cst.KV{"admin.SetParams": "suc"})
 }
